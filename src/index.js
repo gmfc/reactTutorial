9a02630ad7195243ad969ca26acd21cadb5e3d58
gmfc <gabriel.mfcorreia@gmail.com>
2018-09-18 19:58:53 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+
+const API_KEY = 'AIzaSyAiew-vMfAsRY41VYrWIuJXhJI1M-3_GLI'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { videos: [] }
+    YTSearch(
+      { key: API_KEY, term: 'basdasd' },
+      videos => this.setState({ videos }) // resulta em {videos:videos}
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
